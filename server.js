d817cf288e283d490db8745d482f51f4cab3155a
davy-mgr <d.mugire@alustudent.com>
2026-03-14 15:17:54 +0200

    cors update

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+  })
+);
 app.use(express.json());
 
 const userRoutes = require('./routes/usersRoutes');
